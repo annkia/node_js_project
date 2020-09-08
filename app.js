0814a56e39b5b261ec3efed6f68bf3bafcba5479
Anna <Anna.Olender@grandparade.co.uk>
2020-09-08 21:26:29 +0200

    create api request

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const request = require("request");
+const request = require("request-promise");
 
-const url = "https://api.github.com/search/repositories";
+//endpoint
+const url = "https://api.github.com/users";
 
-request({ url: url, json: true }, (error, response) => {
-  console.log("Repositories from github", response);
-});
+//all githubapi requests must include a User-Agent header
+const userAgent =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36";
+
+async function getUsers() {
+  try {
+    const response = await request({
+      url: `${url}/annkia/repos`,
+      method: "GET",
+      headers: {
+        "User-Agent": userAgent,
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+getUsers();
