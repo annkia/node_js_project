23f1a1a7d432b4c9401f993f44322bfe44a470d2
Anna <Anna.Olender@grandparade.co.uk>
2020-09-11 11:45:47 +0200

    change library

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const request = require("request-promise");
+const request = require("postman-request");
 
 //endpoint
 const url = "https://api.github.com/users";
@@ -7,19 +7,22 @@ const url = "https://api.github.com/users";
 const userAgent =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36";
 
-async function getUsers() {
+const options = {
+  url: "https://api.github.com/repos/request/request",
+  headers: {
+    "User-Agent": userAgent,
+  },
+};
+
+function getUsers(error, response, body) {
   try {
-    const response = await request({
-      url: `${url}/annkia/repos`,
-      method: "GET",
-      headers: {
-        "User-Agent": userAgent,
-      },
-    });
-    console.log(response);
+    const info = JSON.parse(body);
+    console.log(
+      `Stars ${info.stargazers_count}, full name ${info.full_name}, url ${info.url}, created ${info.created_at}`
+    );
   } catch (error) {
     console.log(error.message);
   }
 }
 
-getUsers();
+request(options, getUsers);
